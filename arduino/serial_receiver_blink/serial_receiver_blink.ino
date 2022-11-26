c87e3c8bdd9c0b7f4577ef302b040ee4b9e5ae6b
Tilo Reinhardt <tilo.reinhardt@gmail.com>
2022-11-26 12:32:40 +0100

    Accept a parameter and list available serial ports

---
+++ arduino/serial_receiver_blink/serial_receiver_blink.ino
@@ -0,0 +1,35 @@
+#define BUFFER_SIZE 5
+
+byte buffer[BUFFER_SIZE];
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  // Initialize Serial port
+  Serial.begin(57600);
+}
+
+/*
+* Blink 1Hz <times> times
+*/
+void blink(int times) {
+  for (int i = 0; i < times; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+    delay(1000);                      // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+    delay(1000);                      // wait for a second
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int bytes_read = 0;
+  bytes_read = Serial.readBytes(buffer, BUFFER_SIZE);
+  if (bytes_read != 0) {
+    char output[80];
+    for (int b = 0; b < bytes_read; b++) {
+      sprintf(output, "Received(%d): %02x", b, buffer[b]);
+      Serial.println(output);
+    }
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use serialport::*;
+use std::env;
+
+fn get_parameter() -> String {
+    let args: Vec<String> = env::args().collect();
+    if args.len() >= 2 {
+        return args[1].clone();
+    }
+    "TEST".to_string()
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", get_parameter());
+
+    match available_ports() {
+        Ok(ok_var) => println!("Ok {:?}", ok_var),
+        _ => println!("Error"),
+    }
 }
